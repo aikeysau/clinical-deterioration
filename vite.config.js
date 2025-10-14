d9dab9d8953dc4f2b35bdc04cc718533b29264cd
Moni313 <monica.noselli@gmail.com>
2025-10-14 20:49:20 +1000

    config base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/clinical-deterioration/' : '/', 
+  base: process.env.NODE_ENV === 'production' ? '/clinical-deterioration/' : '/',
+  plugins: [vue()], 
 })
