0b968d431c1cf2fb05f92ac5d4d8fd7574a3ce43
Moni313 <monica.noselli@gmail.com>
2025-10-15 06:10:36 +1000

    Fix: add @vitejs/plugin-vue for Vue file support

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/clinical-deterioration/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/clinical-deterioration/' : '/',
   plugins: [vue()], 
+  base: 'clinical-deterioration',
 })
