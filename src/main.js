5d33f69445e600eaf278671ab8f80fea6bd11277
Moni313 <monica.noselli@gmail.com>
2025-10-14 15:49:42 +1000

    fix vue router and base path  for github page

--- src/main.js
+++ src/main.js
@@ -3,4 +3,4 @@ import router from './router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app').use(router).mount('#app')
+createApp(App).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Welcome to Clinical Deterioration App</h1>
+  <div style="text-align:center; margin-top:50px;">
+    <h1>Clinical Deterioration App</h1>
+    <p>Your Vue app is working!</p>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
