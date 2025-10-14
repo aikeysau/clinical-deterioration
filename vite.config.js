c1d54ab16dc6d919bc05b44c6fe2593d8edf4ecf
Moni313 <monica.noselli@gmail.com>
2025-10-15 06:39:10 +1000

    Fix index.html and Vue mount point

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/clinical-deterioration/' : '/',
   plugins: [vue()], 
   base: 'clinical-deterioration',
 })
