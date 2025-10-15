e8bd5ec1b63c24ec611ea2bfe1724443455c7f88
Moni313 <monica.noselli@gmail.com>
2025-10-15 17:57:49 +1000

    updating About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,8 +5,13 @@
   <section>
     <h3>About AI-KEYS</h3>
     <p>
-      AI-KEYS is a research initiative focused on clinical decision support and
+      AI-KEYS is a Clinical Deterioration Knowledge Hub and research initiative focused on clinical decision support and
       early detection of patient deterioration using artificial intelligence.
+      AI-KEYS is a group of medical informatics professionals, clinicians and computer science researchers, sharing understanding, and actionable insights. The goal is to bridge the gap between clinical expertise and technical implementation.
+      <br></br>
+      We are interested in clinical context, data and metrics, AI algorithsm insights, user interface and human factors, practical implementation.
+      <br></br>
+      We take into account collaboration and workflow, governance and ethics, 
     </p>
   </section>
 </template>
